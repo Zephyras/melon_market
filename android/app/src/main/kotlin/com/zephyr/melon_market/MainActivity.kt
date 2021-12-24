85c708755f86909216724cd8ab05bf65e9575921
황교욱 <hkw0101@gmail.com>
2021-12-24 23:59:30 +0900

    첫번째 커밋

---
+++ android/app/src/main/kotlin/com/zephyr/melon_market/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zephyr.melon_market
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(
+          color: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
