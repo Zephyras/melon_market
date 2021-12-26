cb5230390ae8b05ea7177ad2673257b0cb4c9d8d
황교욱 <hkw0101@gmail.com>
2021-12-26 10:51:31 +0900

    AnimatedSiwtcher()위젯사용/분기에따라예외처리/logger플러그인추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:melon_market/splash_screen.dart';
+import 'package:melon_market/utils/logger.dart';
 
 void main() {
+  logger.d('My first log by logger!');
   runApp(MyApp());
 }
 
@@ -9,8 +11,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
-      home: SplashScreen(),
+    return FutureBuilder<Object>(
+        future: Future.delayed(Duration(seconds: 3), () => 300),
+        builder: (context, snapshot) {
+          return AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              child: _splashLodingWidget(snapshot));
+        });
+  }
+
+  StatelessWidget _splashLodingWidget(AsyncSnapshot<Object> snapshot) {
+    if (snapshot.hasError) {
+      print('error occur while loading.');
+      return Text('Error occur');
+    } else if (snapshot.hasData) {
+      return melonApp();
+    } else {
+      return SplashScreen();
+    }
+  }
+}
+
+class melonApp extends StatelessWidget {
+  const melonApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
     );
   }
 }
